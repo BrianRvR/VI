8eb85f634f714cc1870402daacf14d1466c1ee44
BrianRvR <98058479+BrianRvR@users.noreply.github.com>
2022-12-12 10:01:19 +0100

    Update for.php

---
+++ Contactofor/for.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+    Hola <?php echo $_POST["nombres"]; ?><br>
+    Tu Apellido es: <?php echo $_POST["apellidos"]; ?>
+    Tu numero es:<?php echo $_POST["telefono"]; ?>
+    Tu correo es:<?php echo $_POST["correo"]; ?>
+    </body>
+</html>
