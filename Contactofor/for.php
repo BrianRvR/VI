012e4bc12c325c35e811656713d4bc05673c50fc
BrianRvR <98058479+BrianRvR@users.noreply.github.com>
2022-12-12 10:30:36 +0100

    Update for.php

--- Contactofor/for.php
+++ Contactofor/for.php
@@ -1,12 +1,1 @@
-<html>
-    <body>
-   
-<?php
-	if(isset($_POST["enviar"]) && !empty($_post["enviar"])) {
-		$nombre = $_post["nombre"];
-		echo "<b>" .$nombre . "</b>";
-	}
-?>
 
-    </body>
-</html>
